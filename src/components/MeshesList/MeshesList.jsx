f6410136233e583f2fb403bd9d572024da51319a
Guillaume Peres <guillaumeperes10@gmail.com>
2017-10-24 22:13:37 +0200

    Affichage dynamique des fichiers de maillage

--- src/components/MeshesList/MeshesList.jsx
+++ src/components/MeshesList/MeshesList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Component } from "react";
 import { Header } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import { Grid } from "semantic-ui-react";
 import { Label } from "semantic-ui-react";
 import { Image } from "semantic-ui-react";
@@ -8,6 +9,9 @@ import { Button } from "semantic-ui-react";
 import { Icon } from "semantic-ui-react";
 import { Loader } from "semantic-ui-react";
 import { Dimmer } from "semantic-ui-react";
+import { baseApiUrl } from "../../conf";
+import axios from "axios";
+import filesize from "filesize";
 import "./MeshesList.css";
 
 export default class MeshesList extends Component {
@@ -15,48 +19,91 @@ export default class MeshesList extends Component {
         super(props);
         this.state = {
             "isLoading": true,
-            "meshes": [{}, {}, {}]
+            "meshes": []
         };
     }
 
     componentDidMount() {
-        this.setState(Object.assign({}, this.state, {
-            "isLoading": false
-        }));
+        const self = this;
+        const route = baseApiUrl + "/meshes/search/";
+        axios.get(route).then(function(response) {
+            self.setState({
+                "isLoading": false,
+                "meshes": response.data
+            });
+        }).catch(function(error) {
+            console.log(error); // Todo : redirection vers une page d'erreur
+        });
+    }
+
+    renderMeshTags(mesh) {
+        if (mesh.tags.length > 0) {
+            const tags = mesh.tags.map(function(tag, i) {
+                return <Label key={i} as="span" size="small">{tag.title}</Label>
+            });
+            return (
+                <div className="MeshesList-meshTags">{tags}</div>
+            );
+        }
     }
 
     renderMeshes() {
-        let out = this.state.meshes.map(function(mesh, i) {
+        const self = this;
+        const out = self.state.meshes.map(function(mesh, i) {
+            var thumb = (
+                <div className="MeshesList-meshIcon">
+                    <Icon name="image" size="large" color="grey" />
+                </div>
+            );
+            if (typeof mesh.images[0].thumbUri === "string") {
+                thumb = <Image width="70px" height="70px" verticalAlign="middle" alt={mesh.title} shape="rounded" src={baseApiUrl + mesh.images[0].thumbUri} className="MeshesList-meshImage" />;
+            }
+            const d = new Date(mesh.created);
+            const created = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() + " - " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+            const size = filesize(mesh.filesize, {
+                "base": 2,
+                "output": "object"
+            });
+
             return (
                 <Grid.Row key={i} className="MeshesList-mesh">
-                    <Grid.Column width={10}>
-                        <Image bordered={true} width="60px" height="60px" verticalAlign="middle" alt="Titre du fichier de maillage" src="/B1.png" className="MeshesList-meshImage" />
+                    <Grid.Column width={8} verticalAlign="middle">
+                        {thumb}
                         <div className="MeshesList-meshInfo">
-                            <Header as="h2" size="small">Titre du fichier de maillage</Header>
-                            <div className="MeshesList-meshDate">22/10/2017 - 20 : 50</div>
-                            <div className="MeshesList-meshTags">
-                                <Label as="span" size="small">3D</Label>
-                                <Label as="span" size="small">3D surfacique</Label>
-                                <Label as="span" size="small">3D volumique</Label>
-                                <Label as="span" size="small">Hybride</Label>
-                                <Label as="span" size="small">Triangulaire</Label>
-                                <Label as="span" size="small">Immeuble</Label>
-                            </div>
+                            <Header as="h2" size="small">{mesh.title}</Header>
+                            <div className="MeshesList-meshDate">{created}</div>
+                            { self.renderMeshTags(mesh) }
                         </div>
                     </Grid.Column>
-                    <Grid.Column width={3} textAlign="center" verticalAlign="middle">
-                        <div>500 cellules</div>
-                        <div>365 sommets</div>
+                    <Grid.Column width={2} textAlign="center" verticalAlign="middle" className="MeshesList-meshMetadata">
+                        <div><span className="MeshesList-meshDigit">{size.value}</span> {size.suffix}</div>
+                        <div>Type : <span className="MeshesList-meshDigit">{mesh.filetype.toUpperCase()}</span></div>
                     </Grid.Column>
-                    <Grid.Column width={3} textAlign="center" verticalAlign="middle">
-                        <Button icon circular compact basic size="tiny"><Icon name="eye" /></Button>
-                        <Button icon circular compact basic size="tiny"><Icon name="pencil" /></Button>
-                        <Button icon circular compact basic size="tiny"><Icon name="trash" /></Button>
+                    <Grid.Column width={3} textAlign="center" verticalAlign="middle" className="MeshesList-meshMetadata">
+                        <div><span className="MeshesList-meshDigit">{parseInt(mesh.cells, 10).toLocaleString()}</span> cellules</div>
+                        <div><span className="MeshesList-meshDigit">{parseInt(mesh.vertices, 10).toLocaleString()}</span> sommets</div>
+                    </Grid.Column>
+                    <Grid.Column width={3} textAlign="right" verticalAlign="middle">
+                        <Button primary icon circular compact basic size="tiny" onClick={self.openMesh} data-tooltip="Voir"><Icon name="eye" /></Button>
+                        <Button secondary icon circular compact basic size="tiny" onClick={self.editMesh} data-tooltip="Modifier"><Icon name="pencil" /></Button>
+                        <Button negative icon circular compact basic size="tiny" onClick={self.deleteMesh} data-tooltip="Supprimer"><Icon name="trash" /></Button>
                     </Grid.Column>
                 </Grid.Row>
             );
         });
         return out;
+    }
+
+    openMesh() {
+        console.log("Open");
+    }
+
+    editMesh() {
+        console.log("Edit");
+    }
+
+    deleteMesh() {
+        console.log("Delete");
     }
 
     render() {
@@ -68,11 +115,20 @@ export default class MeshesList extends Component {
                     </Dimmer>
                 </Dimmer.Dimmable>
             );
+        } else if (this.state.meshes.length === 0) {
+            return (
+                <div className="MeshesList">
+                    <Container textAlign="center" text>
+                        <Icon name="search" size="huge" color="grey" />
+                        <div>Aucun résulat.</div>
+                    </Container>
+                </div>
+            );
         } else {
             return (
                 <div className="MeshesList">
                     <div className="MeshesList-actions"></div>
-                    <Grid padded={true} columns={3} className="MeshesList-list">
+                    <Grid padded={true} columns={4} className="MeshesList-list">
                         { this.renderMeshes() }
                     </Grid>
                 </div>
