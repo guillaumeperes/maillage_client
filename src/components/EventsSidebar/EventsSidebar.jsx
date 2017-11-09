2e660b9b2e01ee782c5afaec14dfcc6223304912
Guillaume Peres <guillaumeperes10@gmail.com>
2017-11-09 22:44:25 +0100

    Début de la sidebar de droite

--- src/components/EventsSidebar/EventsSidebar.jsx
+++ src/components/EventsSidebar/EventsSidebar.jsx
@@ -2,19 +2,60 @@ import React from "react";
 import { Component } from "react";
 import { Scrollbars } from "react-custom-scrollbars";
 import { Icon } from "semantic-ui-react";
+import { Dimmer } from "semantic-ui-react";
+import { Loader } from "semantic-ui-react";
 import "./EventsSidebar.css";
 
 export default class EventsSidebar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "isLoading": true,
+            "events": []
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            "isLoading": false,
+            "events": []
+        });
+    }
+
     render() {
-        return (
-            <div className="EventsSidebar">
-                <div className="EventsSidebar-title"><Icon name="feed" />Activité</div>
-                <Scrollbars autoHide style={{ width: "100%", height: "calc(100% - 100px)" }}>
-                    <div className="EventsSidebar-list">
-                        contenu
+        if (this.state.isLoading) {
+            return (
+                <div className="EventsSidebar">
+                    <div className="EventsSidebar-title">Activité<Icon name="feed" /></div>
+                    <div className="EventsSidebar-loader">
+                        <Dimmer.Dimmable as="div">
+                            <Dimmer active inverted>
+                                <Loader size="small" inverted>Chargement en cours</Loader>
+                            </Dimmer>
+                        </Dimmer.Dimmable>
                     </div>
-                </Scrollbars>
-            </div>
-        );
+                </div>
+            );
+        } else if (this.state.events.length === 0) {
+            return (
+                <div className="EventsSidebar">
+                    <div className="EventsSidebar-title">Activité<Icon name="feed" /></div>
+                    <div className="EventsSidebar-empty">
+                        Aucune activité n'est répertoriée.
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="EventsSidebar">
+                    <div className="EventsSidebar-title">Activité<Icon name="feed" /></div>
+                    <Scrollbars autoHide style={{ width: "100%", height: "calc(100% - 100px)" }}>
+                        <div className="EventsSidebar-list">
+                            contenu
+                        </div>
+                    </Scrollbars>
+                </div>
+            );
+        }
     }
 }
