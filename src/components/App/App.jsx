98ee2c0bfc80acec31b57a5f9f04d2a119fc65e3
Guillaume Peres <guillaumeperes10@gmail.com>
2017-12-05 20:38:22 +0100

    Reconnexion au rechargement de la page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,40 @@ import React from "react";
 import { Component } from "react";
 import { ToastContainer } from "react-toastify";
 import Router from "../Router/Router";
+import { withCookies } from "react-cookie";
+import PropTypes from "prop-types";
+import axios from "axios";
+import { baseApiUrl } from "../../conf";
+import { connect } from "react-redux";
+import { setUserToken } from "../../actions.js";
 
-export default class App extends Component {
+class App extends Component {
+    static propTypes = {
+        "cookies": PropTypes.object.isRequired,
+    };
+
+    componentDidMount() {
+        const self = this;
+        const cookieToken = self.props.cookies.get("maillage_userToken");
+        if (cookieToken) {
+            const route = baseApiUrl + "/user/revive";
+            const params = {
+                "token": cookieToken
+            };
+            axios.get(route, {"params": params}).then(function(result) {
+                let expire = new Date();
+                expire.setTime(result.data.data.expiresAt * 1000); // on prend la date d'expiration du token fournie par l'api
+                self.props.cookies.set("maillage_userToken", result.data.data.token, {
+                    "path": "/",
+                    "expires": expire
+                });
+                self.props.setUserTokenOnStore(result.data.data.token);
+            }).catch(function(error) {
+                self.props.cookies.remove("maillage_userToken"); // on supprime le cookie parasite
+            });
+        }
+    }
+
     render() {
         return (
             <div>
@@ -13,3 +45,19 @@ export default class App extends Component {
         );
     }
 }
+
+const mapStoreToProps = function(store) {
+    return {};
+};
+
+const mapDispatchToProps = function(dispatch) {
+    return {
+        "setUserTokenOnStore": function(token) {
+            dispatch(setUserToken(token));
+        }
+    };
+};
+
+App = connect(mapStoreToProps, mapDispatchToProps)(App);
+App = withCookies(App);
+export default App;
