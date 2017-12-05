f5d05f2e0ff3b5cc76f05c13a982047ddd7be935
Guillaume Peres <guillaumeperes10@gmail.com>
2017-12-05 21:33:30 +0100

    Menu utilisateur

--- src/components/BarreDuHautAvecLaBareDeRecherche/BarreDuHautAvecLaBareDeRecherche.jsx
+++ src/components/BarreDuHautAvecLaBareDeRecherche/BarreDuHautAvecLaBareDeRecherche.jsx
@@ -3,13 +3,17 @@ import { Component } from "react";
 import { Dropdown, Icon, Menu } from "semantic-ui-react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { removeUserToken } from "../../actions.js";
+import { withCookies } from "react-cookie";
 import "./BarreDuHautAvecLaBareDeRecherche.css";
 
 class BarreDuHautAvecLaBareDeRecherche extends Component {
     static propTypes = {
         "match": PropTypes.object.isRequired,
         "location": PropTypes.object.isRequired,
-        "history": PropTypes.object.isRequired
+        "history": PropTypes.object.isRequired,
+        "cookies": PropTypes.object.isRequired
     };
 
     constructor(props) {
@@ -18,9 +22,24 @@ class BarreDuHautAvecLaBareDeRecherche extends Component {
         this.clicGestUser = this.clicGestUser.bind(this);
         this.clicGestTag = this.clicGestTag.bind(this);
         this.clicLogin = this.clicLogin.bind(this);
+        this.clicLogout = this.clicLogout.bind(this);
     }
 
     render() {
+        let rightMenu = <Menu.Item link onClick={this.clicLogin}><Icon size="large" name="user" /> Se connecter</Menu.Item>;
+        if (this.props.userToken != null) {
+            // Menu utilisateur
+            rightMenu = (
+                <Dropdown item text="Mon compte">
+                    <Dropdown.Menu>
+                        <Dropdown.Item><Icon name="setting" /> Paramètres</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item onClick={this.clicLogout}><Icon name="log out" /> Se déconnecter</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            );
+        }
+
         return (
             <Menu fixed="top" size="small" borderless fluid color="grey" inverted className="BarreDuHautAvecLaBareDeRecherche">
                 <Menu.Item header link onClick={this.clicHome}>
@@ -33,12 +52,16 @@ class BarreDuHautAvecLaBareDeRecherche extends Component {
                     </Dropdown.Menu>
                 </Dropdown>
                 <Menu.Menu position="right">
-                    <Menu.Item link onClick={this.clicLogin}>
-                        <Icon size="large" name="user" /> Se connecter
-                    </Menu.Item>
+                    {rightMenu}
                 </Menu.Menu>
             </Menu>
         );
+    }
+
+    clicLogout(e) {
+        e.preventDefault();
+        this.props.cookies.remove("maillage_userToken");
+        this.props.removeUserTokenOnStore();
     }
 
     clicLogin(e) {
@@ -70,5 +93,21 @@ class BarreDuHautAvecLaBareDeRecherche extends Component {
     }
 }
 
+const mapStoreToProps = function(store) {
+    return {
+        "userToken": store.users.userToken
+    };
+};
+
+const mapDispatchToProps = function(dispatch) {
+    return {
+        "removeUserTokenOnStore": function() {
+            dispatch(removeUserToken());
+        }
+    };
+};
+
+BarreDuHautAvecLaBareDeRecherche = connect(mapStoreToProps, mapDispatchToProps)(BarreDuHautAvecLaBareDeRecherche);
 BarreDuHautAvecLaBareDeRecherche = withRouter(BarreDuHautAvecLaBareDeRecherche);
+BarreDuHautAvecLaBareDeRecherche = withCookies(BarreDuHautAvecLaBareDeRecherche);
 export default BarreDuHautAvecLaBareDeRecherche;
